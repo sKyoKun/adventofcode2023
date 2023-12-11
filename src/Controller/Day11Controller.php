0b88347957ebc607b094470711b674171cb9765c
sKyoKun <sk.kyokun@gmail.com>
2023-12-11 17:09:00 +0100

    [DAY 11] 🪐 Cosmic Expansion (part1)

---
+++ src/Controller/Day11Controller.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Services\CalendarServices;
+use App\Services\InputReader;
+use App\Services\Day11Services;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/day11', name: 'day11')]
+class Day11Controller extends AbstractController
+{
+    public function __construct(
+        private InputReader $inputReader,
+        private Day11Services $day11services,
+        private CalendarServices $calendarServices
+    ){
+    }
+
+    #[Route('/1/{file}', name: 'day11_1', defaults: ["file"=>"day11"])]
+    public function part1(string $file): JsonResponse
+    {
+        $lines = $this->inputReader->getInput($file.'.txt');
+        $initialGrid = $this->calendarServices->parseInputFromStringsToArray($lines);
+        $expandedGrid = $this->day11services->expandGrid($initialGrid);
+        $hashPositions = $this->day11services->getHashPositions($expandedGrid);
+
+        $totalLength = 0;
+        foreach ($hashPositions as $number => $hashPosition) {
+            for($i = $number+1; $i < count($hashPositions); $i++) {
+                $totalLength += $this->day11services->calculateLength($hashPosition, $hashPositions[$i]);
+            }
+        }
+
+        return new JsonResponse($totalLength, Response::HTTP_OK);
+    }
+
+    #[Route('/2/{file}', name: 'day11_2', defaults: ["file"=>"day11"])]
+    public function part2(string $file): JsonResponse
+    {
+        $lines = $this->inputReader->getInput($file.'.txt');
+
+        return new JsonResponse('', Response::HTTP_NOT_ACCEPTABLE);
+    }
+}
---
+++ src/Services/Day11Services.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Services;
+
+class Day11Services
+{
+    public function expandGrid(array $grid): array
+    {
+        // expand vertically
+        $newGrid = $this->expandVertically($grid);
+
+        // expand horizontally
+        $emptyColumns = $this->findEmptyColumns($newGrid);
+        $newGrid = $this->expandHorizontally($newGrid, $emptyColumns);
+
+        return $newGrid;
+    }
+
+    public function expandVertically(array $grid): array
+    {
+        foreach ($grid as $line) {
+            if (false === array_search('#', $line)) {
+                $newGrid[] = $line;
+            }
+            $newGrid[] = $line;
+        }
+
+        return $newGrid;
+    }
+
+    public function expandHorizontally(array $grid, array $emptyColumns): array
+    {
+        foreach ($grid as &$line) {
+            $colToAdd = 0; // For each '.' we add, we also add a column, so oldColumn key should be updated
+            foreach ($emptyColumns as $emptyColumn) {
+                array_splice($line, $emptyColumn+$colToAdd, 0, '.');
+                $colToAdd++;
+            }
+        }
+
+        return $grid;
+    }
+
+    public function findEmptyColumns(array $grid): array
+    {
+        $columns = range(0, count($grid[0]) - 1);
+        foreach ($grid as $line) {
+            foreach ($line as $columnKey => $columnValue) {
+                if (false === array_search($columnKey, $columns) || '.' === $columnValue) {
+                    continue;
+                }
+                // if it's an # then we remove the column from the array to be duplicated
+                $toRemove = array_search($columnKey, $columns);
+                unset($columns[$toRemove]);
+            }
+        }
+
+        return $columns;
+    }
+
+    public function getHashPositions(array $grid): array
+    {
+        $hashPositions = [];
+        foreach ($grid as $lineNumber => $line) {
+            foreach ($line as $colNumber => $value) {
+                if ('#' === $value) {
+                    $hashPositions[] = [$lineNumber, $colNumber];
+                }
+            }
+        }
+
+        return $hashPositions;
+    }
+
+    public function calculateLength(array $pointA, array $pointB): int
+    {
+        return abs($pointB[0]-$pointA[0]) + abs($pointB[1]-$pointA[1]);
+    }
+}
